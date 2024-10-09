c4a30719dedaf30d12e472c026e906bb72cafa9d
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-10-09 11:33:09 +0300

    fix: Add logic for all routes

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,31 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_dict = [bakery.to_dict() for bakery in bakeries]
+    return make_response(bakeries_dict, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id = id).first()
+    bakery_dict = bakery.to_dict()
+    return make_response(bakery_dict, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    # Get all baked goods from the db
+    # Order them price descending
+    # Loop through the goods converting each to dict
+    # Create a response
+    baked_goods = BakedGood.query.order_by(desc(BakedGood.price)).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+    return make_response(baked_goods_list, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    baked_good_dict = baked_good.to_dict()
+    return make_response(baked_good_dict, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
